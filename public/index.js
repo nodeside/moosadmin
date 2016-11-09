023ee0e6ace143eb5e021e439793d2e5b35f76ee
Yehuda <yehuda@nodeside.com>
2016-11-09 15:46:27 +0200

    add/edit filter, sort, change  column size

--- public/index.js
+++ public/index.js
@@ -1,8 +1,15 @@
-var app = angular.module('app', ['ngTouch', 'ui.grid', 'ui.grid.pagination']);
+var app = angular.module('app', ['ngTouch', 'ui.grid',
+   'ui.grid.pagination',
+   'ui.grid.resizeColumns',
+   'ui.grid.moveColumns',
+   'ui.grid.edit',
+   'ui.grid.rowEdit',
+   'ui.grid.cellNav'
+   /*'ui.grid.treeView'*/
+]);
 
-app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridConstants', function($scope, $http, $interval, uiGridConstants) {
+app.controller('MainCtrl', ['$scope', '$http', '$interval', '$q', 'uiGridConstants', function($scope, $http, $interval, $q, uiGridConstants) {
 
-   var columnDefs = [];
    var modelNames = [];
    var modelSelected = '';
    var modelSchema = {};
@@ -24,13 +31,6 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridConstants', f
    init();
 
 
-   // angular.forEach(res, function(value, key) {
-   //       columnDefs = {
-   //         name: value,
-   //         enableFiltering: false
-   //       }
-   //     });
-
    var paginationOptions = {
       pageNumber: 1,
       pageSize: 5,
@@ -43,47 +43,14 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridConstants', f
       useExternalPagination: true,
       useExternalSorting: true,
       enableFiltering: true,
-      useExternalFiltering: true,
-        enableColumnResizing: true,
+      enableColumnResizing: true,
+      //columnDefs: columnDefs,
 
-      columnDefs: columnDefs,
-      // columnDefs: [{
-      //   name: 'name',
-      //   enableFiltering: false,
-      // }, {
-      //   name: 'gender',
-      //   enableSorting: false,
-      //   enableFiltering: false,
-
-      // }, {
-      //   name: 'company',
-      //   enableSorting: false
-      // }],
       onRegisterApi: function(gridApi) {
          $scope.gridApi = gridApi;
-
-         // $scope.gridApi.core.on.filterChanged($scope, function() {
-         //   var grid = this.grid;
-         //   if (grid.columns[1].filters[0].term === 'male') {
-         //     $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/100_male.json')
-         //       .success(function(data) {
-         //         $scope.gridOptions.data = data;
-         //       });
-         //   } else if (grid.columns[1].filters[0].term === 'female') {
-         //     $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/100_female.json')
-         //       .success(function(data) {
-         //         $scope.gridOptions.data = data;
-         //       });
-         //   } else {
-         //     $http.get('https://cdn.rawgit.com/angular-ui/ui-grid.info/gh-pages/data/100.json')
-         //       .success(function(data) {
-         //         $scope.gridOptions.data = data;
-         //       });
-         //   }
-         // });
+         gridApi.rowEdit.on.saveRow($scope, $scope.saveRow);
 
          $scope.gridApi.core.on.sortChanged($scope, function(grid, sortColumns) {
-
             if (sortColumns.length == 0) {
                paginationOptions.sort = null;
             } else {
@@ -93,19 +60,39 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridConstants', f
             getPage();
          });
 
-
          gridApi.pagination.on.paginationChanged($scope, function(newPage, pageSize) {
             paginationOptions.pageNumber = newPage;
             paginationOptions.pageSize = pageSize;
             getPage();
          });
+
+         // $scope.gridApi.treeBase.on.rowExpanded($scope, function(row) {
+         //    if (row.entity.$$hashKey === $scope.gridOptions.data[50].$$hashKey && !$scope.nodeLoaded) {
+         //       $interval(function() {
+         //          $scope.gridOptions.data.splice(51, 0, {
+         //             name: 'Dynamic 1',
+         //             gender: 'female',
+         //             age: 53,
+         //             company: 'Griddable grids',
+         //             balance: 38000,
+         //             $$treeLevel: 1
+         //          }, {
+         //             name: 'Dynamic 2',
+         //             gender: 'male',
+         //             age: 18,
+         //             company: 'Griddable grids',
+         //             balance: 29000,
+         //             $$treeLevel: 1
+         //          });
+         //          $scope.nodeLoaded = true;
+         //       }, 2000, 1);
+         //    }
+         // });
       }
    };
 
    var getPage = function() {
       var url;
-
-      console.log(paginationOptions)
       switch (paginationOptions.sort) {
          case uiGridConstants.ASC:
             url = '/' + $scope.modelSelected + '?sort=' + paginationOptions.sortField + '&order=1';
@@ -118,43 +105,33 @@ app.controller('MainCtrl', ['$scope', '$http', '$interval', 'uiGridConstants', f
             break;
       }
 
-      $http.get(url)
-         .success(function(data) {
-            $scope.gridOptions.columnDefs = [];
+      $http.get(url).success(function(data) {
+         $scope.gridOptions.columnDefs = [];
 
-            for (var key in modelSchema[$scope.modelSelected].fields) {
-               $scope.gridOptions.columnDefs.push({
-                  name: key,
-                  enableColumnResizing:true
-             
-               })
+         for (var key in modelSchema[$scope.modelSelected].fields) {
+            $scope.gridOptions.columnDefs.push({
+               name: key
+            });
+         }
+
+         $scope.gridOptions.totalItems = data.count;
+         var firstRow = (paginationOptions.pageNumber - 1) * paginationOptions.pageSize;
+         $scope.gridOptions.data = data.docs.slice(firstRow, firstRow + paginationOptions.pageSize);
+      });
+
+      $scope.saveRow = function(rowEntity) {
+         // create a fake promise - normally you'd use the promise returned by $http or $resource
+         var promise = $q.defer();
+         $scope.gridApi.rowEdit.setSavePromise(rowEntity, promise.promise);
+
+         // fake a delay of 3 seconds whilst the save occurs, return error if gender is "male"
+         $interval(function() {
+            if (rowEntity.gender === 'male') {
+               promise.reject();
+            } else {
+               promise.resolve();
             }
-
-            /*
-
-[{
-      //   name: 'name',
-      //   enableFiltering: false,
-      // }, {
-      //   name: 'gender',
-      //   enableSorting: false,
-      //   enableFiltering: false,
-
-      // }, {
-      //   name: 'company',
-      //   enableSorting: false
-      // }]
-
-            */
-            // columnDefs = [];
-            //  for (var index in data.docs[0]) {
-            //    columnDefs += '{' + index + '},'
-            // }
-            $scope.gridOptions.totalItems = data.count;
-            var firstRow = (paginationOptions.pageNumber - 1) * paginationOptions.pageSize;
-            $scope.gridOptions.data = data.docs.slice(firstRow, firstRow + paginationOptions.pageSize);
-         });
+         }, 3000, 1);
+      };
    };
-
-   //getPage();
 }]);
